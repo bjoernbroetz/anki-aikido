08b5b1f3536c9b38cf552628716eb26692db25c1
Björn Brötz <no-reply@example.com>
2024-01-26 18:29:03 +0100

    Add ffmpeg commands.

--- video_zerlegen.py
+++ video_zerlegen.py
@@ -1,5 +1,6 @@
 import yaml
 
+INFILE = "data/Aikido-Schule_Bodo-Roedel_1-Kyu-Prüfungsprogramm.mp4"
 
 class AikidoTechnique:
     def __init__(self, standing_position, attack, name, start, end):
@@ -11,7 +12,9 @@ class AikidoTechnique:
 
     def __str__(self):
         return f"{self.standing_position} - {self.attack} - {self.name} ({self.start} to {self.end})"
-
+    
+    def mp4name(self):
+        return f"{self.standing_position.replace(' ','-')}_{self.attack.replace(' ','-')}_{self.name.replace(' ','-')}.mp4"
 
 def read_yaml_file(file_path):
     with open(file_path, 'r') as file:
@@ -37,13 +40,14 @@ def create_aikido_techniques(yaml_data):
 
     return _techniques
 
+def create_ffmpeg_commandline(techniques):
+    for technique in aikido_techniques:
+        # ffmpeg -i input.mp4 -ss 00:05:10 -to 00:15:30 -c:v copy -c:a copy output2.mp4
+        print(f"ffmpeg -i {INFILE} -ss {technique.start} -to {technique.end} -c:v copy -c:a copy {technique.mp4name()}")
 
 if __name__ == "__main__":
     filepath_to_config = "1-kyu_techniken.yaml" 
     yaml_data = read_yaml_file(filepath_to_config)
 
     aikido_techniques = create_aikido_techniques(yaml_data)
-
-    # Print the created instances for demonstration
-    for technique in aikido_techniques:
-        print(technique)
+    create_ffmpeg_commandline(aikido_techniques)
